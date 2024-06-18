72141c50c0d4175d1f223722806daf30bf53c2f6
ayushsingh257 <ayush.singh@kalvium.community>
2024-06-18 12:18:52 +0530

    client-deleted

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+plugins: [react()],
+resolve: {
+alias: {
+'@': '/src',
+},
+},
 });
